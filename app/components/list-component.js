ea0bc3ed6cfdd9ce0ec78da0a1db96bd78e3aca2
Patrick Mounts <patrick@planetargon.com>
2016-04-28 15:51:42 -0700

    remove actions from list-component that were moved inot nav

--- app/components/list-component.js
+++ app/components/list-component.js
@@ -8,16 +8,7 @@ export default Ember.Component.extend({
   generate: 'generate',
   reloadContent: 'reloadContent',
   reloadModel: 'reloadModel',
-  setSearch: 'setSearch',
-  transitionTo: 'transitionTo',
-
-  showProfile: false,
-  // classNameBindings: 'showProfile',
-
-  searchResult: function() {
-    this.sendAction('setSearch', this.get('search'));
-  }.observes('search'),
-
+  
 
   actions: {
     saveNewItem: function() {
@@ -29,21 +20,6 @@ export default Ember.Component.extend({
     removeUser: function(user) {
       this.sendAction('deleteUser', user);
     },
-
-    clearList: function(modelName) {
-      this.sendAction('unloadAll', modelName);
-      this.set('search', '');
-      this.sendAction('transitionTo', 'list');
-    },
-
-    sort: function(param) {
-      this.sendAction('sort', param);
-    },
-
-    generate: function(count) {
-      this.sendAction('generate', count, this.get('modelName'));
-      this.sendAction('reloadModel');
-    }
   }
 
 });
